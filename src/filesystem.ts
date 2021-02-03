8ed5dad737ec4cdaa2d7c344c42faa60f11c33b0
mierenmanz <christiaanmens@gmail.com>
2021-02-03 21:46:30 +0100

    added async fileExist

--- src/filesystem.ts
+++ src/filesystem.ts
@@ -6,3 +6,12 @@ export function fileExistSync(path: string): boolean {
   }
   return true;
 }
+
+export async function fileExist(path: string): Promise<boolean> {
+  try {
+    await Deno.stat(path);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
