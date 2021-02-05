04ff8b47525e1bfd0d41c3f03e42581d20373a93
mierenmanz <christiaanmens@gmail.com>
2021-02-05 17:18:34 +0100

    added extra error and colors

--- src/error.ts
+++ src/error.ts
@@ -1,11 +1,22 @@
 export class NameDuplicationError extends Error {
 
-    public name = "Name Duplication";
+    public name = "\u001b[31;1mNAME DUPLICATION\u001b[0m";
     public message: string;
 
     public constructor(name: string) {
         super();
         this.message = name + " is already used as item name";
+        return;
+    }
+}
+
+export class InternalIDDuplicationError extends Error {
+    public name = "\u001b[31;1mINTERNAL ERROR: ID DUPLICATION\u001b[0m";
+    public message: string;
+
+    public constructor(id: string) {
+        super();
+        this.message = id + " is already used as ID\nThis is an internal error, please make an issue and show your code"
         return;
     }
 }
