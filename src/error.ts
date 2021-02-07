52b11db506f6ff1089e398f7fa5b4d3194ce5eb8
mierenmanz <christiaanmens@gmail.com>
2021-02-07 15:01:12 +0100

    removed unused error

--- src/error.ts
+++ src/error.ts
@@ -9,17 +9,6 @@ export class NameDuplicationError extends Error {
   }
 }
 
-export class InternalIDDuplicationError extends Error {
-  public name = "\u001b[31;1mINTERNAL ERROR: ID DUPLICATION\u001b[0m";
-  public message: string;
-
-  public constructor(id: string) {
-    super();
-    this.message = id +
-      " is already used as ID\nThis is an internal error, please make an issue and show your code";
-    return;
-  }
-}
 export class NameNotFoundError extends Error {
   public name = "\u001b[31;1mNAME NOT FOUND\u001b[0m";
   public message: string;
