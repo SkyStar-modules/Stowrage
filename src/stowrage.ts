dc72a3eeec431269562145a4d1ba6d99e99737b1
mierenmanz <christiaanmens@gmail.com>
2021-02-24 21:22:35 +0100

    added comments for doc.deno.land

--- src/stowrage.ts
+++ src/stowrage.ts
@@ -37,6 +37,7 @@ export class Stowrage<DataType = unknown> {
   public isPersistent: boolean | undefined = false;
 
   /**
+  constructor
   @param { StowrageOptions } options - all start options for stowrage
   */
   public constructor(options?: StowrageOptions) {
@@ -68,14 +69,25 @@ export class Stowrage<DataType = unknown> {
     return;
   }
 
-  public ensure(name: string, key: DataType): DataBase<DataType> {
+  /**
+  Add entry to db and returns it aswell
+  @param { string } name - Name for the entry
+  @param { DataType } value - Value that you want to store (Can be anything)
+  @returns { DataBase<DataType> } Returns entry after it has been added
+  */
+  public ensure(name: string, value: DataType): DataBase<DataType> {
     if (this.#DB.has(name)) throw new NameDuplicationError(name);
-    const entry = this.generateEntry(name, key);
+    const entry = this.generateEntry(name, value);
     this.#IDMap.set(entry.id, name);
     this.#DB.set(name, entry);
     return entry;
   }
 
+  /**
+  Add entry to db
+  @param { string } name - Name for the entry
+  @param { DataType } value - Value that you want to store (Can be anything)
+  */
   public add(name: string, key: DataType): void {
     if (this.#DB.has(name)) throw new NameDuplicationError(name);
     const entry = this.generateEntry(name, key);
@@ -84,6 +96,10 @@ export class Stowrage<DataType = unknown> {
     return;
   }
 
+  /**
+  Delete entry from db by name
+  @param { string } name - Name of the entry you want to delete
+  */
   public delete(name: string): void {
     const entry = this.#DB.get(name);
     if (!entry) throw new NameNotFoundError(name);
@@ -95,6 +111,10 @@ export class Stowrage<DataType = unknown> {
     return;
   }
 
+  /**
+  Delete entry from db by ID
+  @param { number } id - ID of the entry you want to delete
+  */
   public deleteByID(id: number): void {
     const name = this.#IDMap.get(id);
     if (!name) throw new IDNotFoundError(id);
@@ -106,6 +126,11 @@ export class Stowrage<DataType = unknown> {
     return;
   }
 
+  /**
+  Delete multiple entries from db
+  @param { number } start - First entry you want to delete
+  @param { number } length - Length to delete
+  */
   public deleteByRange(start: number, length: number): void {
     const range = start + length;
     if (range >= this.#DB.size) {
@@ -136,9 +161,14 @@ export class Stowrage<DataType = unknown> {
     return;
   }
 
-  public override(name: string, key: DataType): void {
+  /**
+  Override entry from db by name
+  @param { string } name - Name of the entry you want to delete
+  @param { DataType } newValue - New value you want to store
+  */
+  public override(name: string, newValue: DataType): void {
     if (!this.#DB.has(name)) throw new NameNotFoundError(name);
-    const entry: DataBase<DataType> = this.generateEntry(name, key, true);
+    const entry: DataBase<DataType> = this.generateEntry(name, newValue, true);
     this.#DB.set(name, entry);
     if (this.#SQLDB && this.isPersistent) {
       this.#SQLDB.query(
@@ -149,6 +179,11 @@ export class Stowrage<DataType = unknown> {
     return;
   }
 
+  /**
+  Override entry from db by name
+  @param { string } name - Name of the entry you want to delete
+  @param { DataType } newValue - New value you want to store
+  */
   public overrideByID(id: number, key: DataType): void {
     const name: string | undefined = this.#IDMap.get(id);
     if (name) {
@@ -159,26 +194,52 @@ export class Stowrage<DataType = unknown> {
     return;
   }
 
+  /**
+  Get an Array of entries that matches your filter
+  @param { FilterFunc<DataType> } func - FilterFunction
+  @returns { DataBase<DataType>[] } - Returns Array with filtered entries
+  */
   public filter(func: FilterFunc<DataType>): DataBase<DataType>[] {
     return [...this.#DB.values()].filter(func);
   }
 
+  /**
+  Get the first entry that matches your filter
+  @param { FilterFunc<DataType> } func - FilterFunction
+  @returns { DataBase<DataType> | undefined } - Returns first match found or undefined if no match is found
+  */
   public find(func: FilterFunc<DataType>): DataBase<DataType> | undefined {
     return [...this.#DB.values()].find(func);
   }
 
+  /**
+  Fetch entry by name
+  @param { string } name - Name of the entry you want to fetch
+  @returns { DataBase<DataType> } - Returns the entry that matches the name or throws
+  */
   public fetch(name: string): DataBase<DataType> {
     const data = this.#DB.get(name);
     if (!data) throw new NameNotFoundError(name);
-    else return data;
+    return data;
   }
 
+  /**
+  Fetch entry by ID
+  @param { number } id - ID of the entry you want to fetch
+  @returns { DataBase<DataType> } - Returns the entry that matches the ID or throws
+  */
   public fetchByID(id: number): DataBase<DataType> {
     const name: string | undefined = this.#IDMap.get(id);
     if (!name) throw new IDNotFoundError(id);
-    else return this.#DB.get(name)!;
+    return this.fetch(name);
   }
 
+  /**
+  Fetch multiple entries from db
+  @param { number } start - First entry you want to fetch
+  @param { number } length - Length to fetch
+  @returns { DataBase<DataType>[] } - Returns Array 
+  */
   public fetchByRange(start: number, length: number): DataBase<DataType>[] {
     const range = start + length;
     if (range >= this.#DB.size) return [...this.#DB.values()];
@@ -187,7 +248,18 @@ export class Stowrage<DataType = unknown> {
     );
   }
 
+  /**
+  Set the value of an entry via ID
+  @param { number } id - ID of the entry to change value of
+  @param { ChangeValueOptions<unknown> } options - Extra options like key and value
+  */
   public setValue(id: number, options: ChangeValueOptions<unknown>): void;
+
+  /**
+  Set the value of an entry via ID
+  @param { string } name - Name of the entry to change value of
+  @param { ChangeValueOptions<unknown> } options - Extra options like key and value
+  */
   public setValue(name: string, options: ChangeValueOptions<unknown>): void;
   public setValue(
     nameID: string | number,
@@ -215,6 +287,11 @@ export class Stowrage<DataType = unknown> {
     return;
   }
 
+  /**
+  Check if your stowrage has an entry with the given name
+  @param { string } searchName - Name to look for
+  @returns { boolean } - Returns true or false
+  */
   public has(name: string): boolean {
     return this.#DB.has(name);
   }
@@ -229,6 +306,9 @@ export class Stowrage<DataType = unknown> {
     return;
   }
 
+  /**
+  Close SQLite database if it exists
+  */
   public close(): void {
     if (!this.#SQLDB) {
       throw new nonPersistentError(this.name ?? "unnamed db", "closed");
@@ -237,6 +317,9 @@ export class Stowrage<DataType = unknown> {
     return;
   }
 
+  /**
+  Get total entries of the Stowrage
+  */
   public totalEntries(): number {
     return this.#DB.size;
   }
@@ -257,18 +340,19 @@ export class Stowrage<DataType = unknown> {
       name: name,
       data: key,
     };
-    if (this.#SQLDB && this.isPersistent && !override) {
-      this.#SQLDB.query(
-        "INSERT INTO stowrage (id, name, data) VALUES(?, ?, ?)",
-        [obj.id, obj.name, JSON.stringify(obj.data)],
-      );
-    }
-
-    if (this.maxEntries && this.#DB.size > this.maxEntries) {
-      const name = this.#IDMap.get(this.#id - this.maxEntries);
-      if (name) this.#DB.delete(name);
+    if (!override) {
       if (this.#SQLDB && this.isPersistent) {
-        this.#SQLDB.query("DELETE FROM stowrage WHERE name = ?", [name]);
+        this.#SQLDB.query(
+          "INSERT INTO stowrage (id, name, data) VALUES(?, ?, ?)",
+          [obj.id, obj.name, JSON.stringify(obj.data)],
+        );
+      }
+      if (this.maxEntries && this.#DB.size > this.maxEntries) {
+        const name = this.#IDMap.get(this.#id - this.maxEntries);
+        if (name) this.#DB.delete(name);
+        if (this.#SQLDB && this.isPersistent) {
+          this.#SQLDB.query("DELETE FROM stowrage WHERE name = ?", [name]);
+        }
       }
     }
     return obj;
