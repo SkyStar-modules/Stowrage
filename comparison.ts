8f0cc8b3f9ea6aa50a11a03c8c272e11d838c9e2
MierenManz <christiaanmens@gmail.com>
2021-04-10 23:21:25 +0200

    bench-branch back

--- comparison.ts
+++ comparison.ts
@@ -1,28 +1,21 @@
 import { Stowrage } from "./mod.ts";
-import { Stowrage as oldStow } from "https://deno.land/x/stowrage/mod.ts";
-import { Keydb } from "https://deno.land/x/keydb@1.0.0/sqlite.ts";
-import { DB as SQLite } from "https://deno.land/x/sqlite@v2.3.2/mod.ts";
+import { Keydb } from "https://deno.land/x/keydb/mod.ts";
 import { render } from "https://deno.land/x/eta/mod.ts";
 
-const total = (Deno.args[0]) ? parseInt(Deno.args[0]) : 10000;
-const step = (Deno.args[1]) ? parseInt(Deno.args[1]) : 1000;
+const total = Deno.args[0] ? parseInt(Deno.args[0]) : 10000;
+const step = Deno.args[1] ? parseInt(Deno.args[1]) : 100;
 interface interfaceThing {
   totalTime: number[];
   avg: number[];
 }
 interface Thing {
   entries: number[];
-  oldStowrage: interfaceThing;
   stowrage: interfaceThing;
   keyDB: interfaceThing;
-  SQLite: interfaceThing;
 }
 const obj: Thing = {
   entries: [],
-  oldStowrage: {
-    totalTime: [],
-    avg: [],
-  },
+
   stowrage: {
     totalTime: [],
     avg: [],
@@ -31,21 +24,14 @@ const obj: Thing = {
     totalTime: [],
     avg: [],
   },
-  SQLite: {
-    totalTime: [],
-    avg: [],
-  },
 };
-
 for (let i = step; i <= total; i += step) {
-  console.log(i);
+  gc();
   obj.entries.push(i);
-  const ostow = new oldStow();
   const stow = new Stowrage();
   const keydb = new Keydb();
-  const sql = new SQLite();
-  sql.query("CREATE TABLE IF NOT EXISTS sql (id INTEGER, name TEXT)");
   // stowrage
+
   let start = performance.now();
   for (let j = 0; j <= i; j++) {
     stow.add(`${j}`, j);
@@ -54,16 +40,7 @@ for (let i = step; i <= total; i += step) {
   let math = end - start;
   obj.stowrage.totalTime.push(math);
   obj.stowrage.avg.push(math / i);
-
-  // stowrage
-  start = performance.now();
-  for (let j = 0; j <= i; j++) {
-    await ostow.add(`${j}`, j);
-  }
-  end = performance.now();
-  math = end - start;
-  obj.oldStowrage.totalTime.push(math);
-  obj.oldStowrage.avg.push(math / i);
+  gc();
 
   // KeyDB
   start = performance.now();
@@ -74,16 +51,7 @@ for (let i = step; i <= total; i += step) {
   math = end - start;
   obj.keyDB.totalTime.push(math);
   obj.keyDB.avg.push(math / i);
-
-  // SQLite
-  start = performance.now();
-  for (let j = 0; j <= i; j++) {
-    sql.query("INSERT INTO sql (name, id) VALUES(?, ?)", [`${j}`, j]);
-  }
-  end = performance.now();
-  math = end - start;
-  obj.SQLite.totalTime.push(math);
-  obj.SQLite.avg.push(math / i);
+  gc();
 }
 
 const ejs = Deno.readTextFileSync("graph.ejs");
